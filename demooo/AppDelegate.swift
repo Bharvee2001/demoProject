6aacbf54075f36e33c0bf222103c1af304af343a
bharvee2001 <bharvee.parmar@gmail.com>
2019-11-27 00:22:42 +0530

    App Delegate Changes

--- demooo/AppDelegate.swift
+++ demooo/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // this is used for working view controller method
         return true
     }
 
